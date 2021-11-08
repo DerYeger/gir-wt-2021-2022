1a86ba48bea5d0a1869139a6bdad28251ab041cd
Jan Müller <janmueller3698@gmail.com>
2021-11-08 10:52:38 +0100

    feat: improve cli

--- code/createindex.py
+++ code/createindex.py
@@ -1,15 +1,36 @@
-import time
-
 from inverted_index import InvertedIndex
-from query import query
+from prompt_toolkit.validation import Validator, ValidationError
+from PyInquirer import prompt
 
 _index_dir: str = './tables'
 _dataset_dir: str = './dataset/articles'
 
 
-def map_dict(f, dic: dict) -> dict:
-    return dict(zip(dic, map(f, dic.values())))
-
-
-def get_index(load_from_disk: bool, max_files) -> InvertedIndex:
+def get_index(load_from_disk) -> InvertedIndex:
+    max_files = -1
+    if not load_from_disk:
+        indexing_answers = prompt(_indexing_questions)
+        max_files = indexing_answers.get('max_files')
+        print()
     return InvertedIndex(_index_dir, _dataset_dir, max_files, load_from_disk=load_from_disk)
+
+
+class NumberValidator(Validator):
+    def validate(self, document):
+        try:
+            value = int(document.text)
+            return value > 0 or value == -1
+        except ValueError:
+            raise ValidationError(message="Please enter a valid number larger",
+                                  cursor_position=len(document.text))
+
+
+_indexing_questions = [
+    {
+        'type': 'input',
+        'name': 'max_files',
+        'message': 'Amount of files to index (-1 for all files)',
+        'validate': NumberValidator,
+        'filter': lambda val: int(val)
+    }
+]
--- code/main.py
+++ code/main.py
@@ -4,7 +4,6 @@ import sys
 from createindex import get_index
 from evaluation_mode import run_evaluation_mode
 from exploration_mode import run_exploration_mode
-from prompt_toolkit.validation import Validator, ValidationError
 from PyInquirer import prompt
 
 
@@ -13,55 +12,33 @@ def main():
     args = sys.argv[1:]
     load_from_disk = '-c' not in args and '--clean' not in args
     print()
-    max_files = -1
-    if not load_from_disk:
-        indexing_answers = prompt(_indexing_questions)
-        max_files = indexing_answers.get('max_files')
-        print()
-    index = get_index(load_from_disk, max_files)
+    index = get_index(load_from_disk)
     while True:
         print()
-        answers = prompt(_search_questions)
+        answers = prompt(_questions)
         print()
-        search_mode = answers.get('search_mode')
-        if search_mode == _evaluation_mode_name:
+        action = answers.get('action')
+        if action == _evaluation_action:
             run_evaluation_mode(index)
-        elif search_mode == _exploration_mode_name:
+        elif action == _exploration_action:
             run_exploration_mode(index)
-        elif search_mode == _exit_mode:
+        elif action == _reset_index_action:
+            index = get_index(load_from_disk=False)
+        elif action == _exit_action:
             exit(0)
 
 
-class NumberValidator(Validator):
-    def validate(self, document):
-        try:
-            value = int(document.text)
-            return value > 0 or value == -1
-        except ValueError:
-            raise ValidationError(message="Please enter a valid number larger",
-                                  cursor_position=len(document.text))
+_evaluation_action = 'Evaluation'
+_exploration_action = 'Exploration'
+_reset_index_action = 'Reset index'
+_exit_action = 'Exit'
 
-
-_indexing_questions = [
-    {
-        'type': 'input',
-        'name': 'max_files',
-        'message': 'Amount of files to index (-1 for all files)',
-        'validate': NumberValidator,
-        'filter': lambda val: int(val)
-    }
-]
-
-_evaluation_mode_name = 'evaluation'
-_exploration_mode_name = 'exploration'
-_exit_mode = 'exit'
-
-_search_questions = [
+_questions = [
     {
         'type': 'list',
-        'name': 'search_mode',
-        'message': 'Select search mode',
-        'choices': [_evaluation_mode_name, _exploration_mode_name, _exit_mode]
+        'name': 'action',
+        'message': 'Select action',
+        'choices': [_evaluation_action, _exploration_action, _reset_index_action, _exit_action]
     },
 ]
 
