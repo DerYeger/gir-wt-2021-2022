4d97f9c1c1cd9769d78fbccddbcc4a76211b288f
Jan Müller <janmueller3698@gmail.com>
2021-11-12 17:52:56 +0100

    fix: use correct scoring functions

--- code/scoring.py
+++ code/scoring.py
@@ -31,7 +31,6 @@ def _bm25(index: InvertedIndex, query_tokens: [str]):
             nominator = frequency * (k + 1)
             denominator = frequency + k * (1 - b + (b * (word_count / index.get_average_word_count())))
             article_scores[id_string] += token_idf * (nominator / denominator)
-            article_scores[id_string] += token_idf * frequency
     return article_scores
 
 
@@ -41,15 +40,13 @@ def _tf_idf(index: InvertedIndex, query_tokens: [str]):
         token_idf = _idf(index, token)
         for (article_id, frequency) in index.get_entries_for_token(token):
             id_string = str(article_id)
+            article_tf = np.log(1 + frequency)
             if id_string not in article_scores:
                 article_scores[id_string] = 0
-            article_scores[id_string] += token_idf * frequency
+            article_scores[id_string] += token_idf * article_tf
     return article_scores
 
 
 def _idf(index: InvertedIndex, token: str):
     article_count = index.get_article_count()
-    n_qi = len(index.get_entries_for_token(token))
-    nominator = article_count - n_qi + 0.5
-    denominator = n_qi + 0.5
-    return np.log((nominator / denominator) + 1)
+    return np.log(article_count / (len(index.get_entries_for_token(token)) + 1))
