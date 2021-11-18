887993fd4927412df44dc4ccbb481aa5a9a66d6e
Jan Müller <janmueller3698@gmail.com>
2021-11-18 22:10:54 +0100

    feat: improve tokenization

--- code/tokenizer.py
+++ code/tokenizer.py
@@ -3,7 +3,7 @@ import re
 from nltk import download
 from nltk.stem import SnowballStemmer, StemmerI
 from nltk.corpus import stopwords
-from typing import Dict, List
+from typing import Callable, Dict, List
 
 download('stopwords', quiet=True)
 _stop_words: List[str] = stopwords.words('english')
@@ -11,24 +11,41 @@ _stop_words: List[str] = stopwords.words('english')
 _stemmer: StemmerI = SnowballStemmer('english')
 _stemmer_cache: Dict[str, str] = {}
 
+_punctuation_pattern = re.compile(r'[.,():;\n\t\s \\|/?]')
+_non_word_pattern = re.compile(r'[\Wˆ_]')
+
 
 def tokenize(text: str) -> List[str]:
     """
     :param text: a text string
     :return: an array of tokens with preprocessing (e.g. stemming, stopword removal, ...) applied
     """
-    # replace non-word chars with spaces and split at spaces
-    raw_tokens: List[str] = re.sub(r'[\Wˆ_]+', ' ', text).strip().lower().split()
-    filtered_tokens: filter = filter(lambda t: t not in _stop_words, raw_tokens)
-    stemmed_tokens: map = map(_stem, filtered_tokens)
-    # normalized_tokens = map(replace_special_letters, stemmed_tokens)
+    raw_tokens: List[str] = _punctuation_pattern.split(text.lower().strip())
+    processed_tokens = _flat_map(_process_token, raw_tokens)
+    filtered_tokens = filter(_token_filter, processed_tokens)
+    stemmed_tokens = map(_stem, filtered_tokens)
     return list(stemmed_tokens)
+
+
+def _flat_map(transform: Callable[[str], List[str]], tokens: List[str]) -> List[str]:
+    return [result for token in tokens for result in transform(token)]
+
+
+def _process_token(word: str) -> [str]:
+    parts = word.split('-')
+    if len(parts) > 1:
+        parts.append(word)
+    return [_non_word_pattern.sub('', part) for part in parts]
 
 
 def _stem(word: str) -> str:
     if word not in _stemmer_cache:
         _stemmer_cache[word] = _stemmer.stem(word)
     return _stemmer_cache[word]
+
+
+def _token_filter(token: str) -> bool:
+    return token not in _stop_words and (len(token) > 2 or token.isnumeric())
 
 
 def _replace_special_letters(word: str) -> str:
