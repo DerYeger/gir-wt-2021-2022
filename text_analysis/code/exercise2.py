9043455e3a7e80f277cdb575fdda9db31298e56a
Jan Müller <janmueller3698@gmail.com>
2021-12-20 13:52:24 +0100

    feat(text_analysis): calculate most similar words

--- text_analysis/code/exercise2.py
+++ text_analysis/code/exercise2.py
@@ -1,3 +1,26 @@
-"""
-Add the code for the 2nd exercise to this file. You can add additional ".py" files to your code repository (e.g. helper functions, etc.).
-"""
+import gensim
+import time
+
+model_path = '../model/wiki-news-300d-1M-subword.vec'
+print('Loading model')
+start_time = time.time()
+model = gensim.models.keyedvectors.load_word2vec_format(model_path)
+print(f'Model loaded in {round(time.time() - start_time, 2)} seconds\n')
+
+
+def get_three_most_similar(word: str):
+    print(f'Words similar to "{word}":')
+    results = model.most_similar(word, topn=3)
+    for i, (otherWord, score) in enumerate(results):
+        print(f'{i + 1}. {otherWord} ({score})')
+    print()
+
+
+def main():
+    get_three_most_similar('Vienna')
+    get_three_most_similar('Austria')
+    get_three_most_similar('cat')
+
+
+if __name__ == '__main__':
+    main()
