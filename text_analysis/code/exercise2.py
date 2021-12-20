96994df2ae11eb5cc9934bb44786280fe34178ed
Jan Müller <janmueller3698@gmail.com>
2021-12-20 14:06:53 +0100

    feat(text_analysis): add cosine similarity calculation

--- text_analysis/code/exercise2.py
+++ text_analysis/code/exercise2.py
@@ -1,11 +1,17 @@
 import gensim
 import time
+from typing import Tuple
 
 model_path = '../model/wiki-news-300d-1M-subword.vec'
 print('Loading model')
 start_time = time.time()
 model = gensim.models.keyedvectors.load_word2vec_format(model_path)
 print(f'Model loaded in {round(time.time() - start_time, 2)} seconds\n')
+
+
+def get_cosine_similarity(pair: Tuple[str, str]):
+    result = model.similarity(pair[0], pair[1])
+    print(f'cos_sim({pair[0]}, {pair[1]}) = {result}\n')
 
 
 def get_three_most_similar(word: str):
@@ -17,9 +23,11 @@ def get_three_most_similar(word: str):
 
 
 def main():
-    get_three_most_similar('Vienna')
-    get_three_most_similar('Austria')
-    get_three_most_similar('cat')
+    for pair in [('cat', 'dog'), ('cat', 'Vienna'), ('Vienna', 'Austria'), ('Austria', 'dog')]:
+        get_cosine_similarity(pair)
+
+    for word in ['Vienna', 'Austria', 'cat']:
+        get_three_most_similar(word)
 
 
 if __name__ == '__main__':
