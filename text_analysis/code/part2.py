5fd8f22128473042f4890f908e715b66ced81a64
Jan Müller <janmueller3698@gmail.com>
2021-12-27 15:39:50 +0100

    feat(text_analysis): use better dataset

--- text_analysis/code/part2.py
+++ text_analysis/code/part2.py
@@ -1,5 +1,5 @@
 def main():
-    print("TODO")
+    print("TODO\n")
 
 
 if __name__ == '__main__':
--- text_analysis/code/part3.py
+++ text_analysis/code/part3.py
@@ -1,13 +1,16 @@
 import gensim
-import json
 import multiprocessing
 import os
 import time
 
-from gensim.models import Word2Vec
+from gensim.models import KeyedVectors, Word2Vec
 from gensim.models.callbacks import CallbackAny2Vec
 from gensim.parsing.preprocessing import *
+from shared import get_three_most_similar
 from typing import List
+
+_dataset_dir = './german/dataset'
+_model_file_path = './german/model.vec'
 
 
 class ProgressLogger(CallbackAny2Vec):
@@ -29,13 +32,11 @@ def get_texts_from_dataset() -> List[str]:
     print('Loading dataset')
     start_time = time.time()
     texts = []
-    files_names: List[str] = os.listdir('./german_dataset')
+    files_names: List[str] = os.listdir(_dataset_dir)
     for file_name in files_names:
-        with open(f'./german_dataset/{file_name}') as file:
-            tweets = json.load(file)
-            for tweet in tweets:
-                if 'text' in tweet:
-                    texts.append(tweet['text'])
+        with open(f'{_dataset_dir}/{file_name}', encoding='utf-8') as file:
+            lines = file.read().split('\n')
+            texts.extend(lines)
     print(f'Dataset loaded in {round(time.time() - start_time, 2)} seconds')
     print_size_of_texts(texts)
     return texts
@@ -48,33 +49,55 @@ def print_size_of_texts(texts: List[str]):
     print(f'Dataset size: {round(size / (1024 * 1024 * 1024), 2)}GB\n')
 
 
-def train_model(texts: List[str]) -> Word2Vec:
+def train_model(texts: List[str]) -> KeyedVectors:
     print('Training model')
     num_threads = multiprocessing.cpu_count()
     num_workers = 1 if num_threads <= 1 else num_threads - 1
-    print(f'{multiprocessing.cpu_count()} threads available. Using {num_workers}.')
+    print(f'Using {num_workers} of {multiprocessing.cpu_count()} available threads')
     start_time = time.time()
-    filters = [strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, strip_short]
-    texts_tokenized = [preprocess_string(text.lower(), filters) for text in texts]
 
     epochs = 10
+    texts_tokenized = process_texts(texts)
     german_model = gensim.models.Word2Vec(sentences=texts_tokenized, vector_size=100, window=5, min_count=1,
                                           epochs=epochs, workers=num_workers, callbacks=[ProgressLogger(epochs)])
     print(f'Model trained in {round(time.time() - start_time, 2)} seconds\n')
-    return german_model
+    return german_model.wv
 
 
-def save_model(model: Word2Vec):
+def save_model(model: KeyedVectors):
     print('Saving model')
     start_time = time.time()
-    model.save('../model/german-tweets.vec')
+    model.save_word2vec_format(_model_file_path)
     print(f'Model saved in {round(time.time() - start_time, 2)} seconds\n')
 
 
+def load_model() -> KeyedVectors:
+    print('Loading model')
+    start_time = time.time()
+    model = gensim.models.keyedvectors.load_word2vec_format(_model_file_path)
+    print(f'Model loaded in {round(time.time() - start_time, 2)} seconds\n')
+    return model
+
+
+def get_model() -> KeyedVectors:
+    if os.path.exists(_model_file_path):
+        return load_model()
+    else:
+        texts = get_texts_from_dataset()
+        model = train_model(texts)
+        save_model(model)
+        return model
+
+
+def process_texts(texts: List[str]) -> List[str]:
+    filters = [strip_tags, strip_punctuation, strip_multiple_whitespaces, strip_numeric, strip_short]
+    return [preprocess_string(text.lower(), filters) for text in texts]
+
+
 def main():
-    texts = get_texts_from_dataset()
-    model = train_model(texts)
-    save_model(model)
+    model = get_model()
+    for [word] in process_texts(['Deutschland', 'Politik', 'Kanzler']):
+        get_three_most_similar(word, model)
 
 
 if __name__ == '__main__':
